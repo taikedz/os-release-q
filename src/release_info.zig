53bb3c687c1dd99397ca0f5ec7af7b4c6f9daa08
Tai Kedzierski <dch.tai@gmail.com>
2024-10-05 18:21:41 +0100

    Some working tests on release_info.zig

--- src/release_info.zig
+++ src/release_info.zig
@@ -1,55 +1,115 @@
+const std = @import("std");
+
 pub const LineError = error {
     NotFound,
     NotValid
 };
 
 pub const OsInfo = struct {
-    id:[]u8,
-    version_id:[]u8,
-    pretty_name:[]u8,
-    id_like:[]u8,
+    _alloc:std.mem.Allocator,
+    id:*[]const u8,
+    version_id:*[]const u8,
+    pretty_name:*[]const u8,
+    id_like:*[]const u8,
 
-    alloc:std.mem.Allocator,
-
-    fn init(self:*OsInfo, alloc:std.head.Allocator, ) OsInfo {
-        self.alloc = alloc;
-        return self;
-    },
-
-    fn deinit(self:*OsInfo) {
-        _ = self.alloc.free(self.id);
-        _ = self.alloc.free(self.version_id);
-        _ = self.alloc.free(self.pretty_name);
-        _ = self.alloc.free(self.id_like);
+    pub fn new(alloc:std.mem.Allocator, id:[]const u8, version_id:[]const u8, pretty_name:[]const u8, id_like:[]const u8) OsInfo {
+        return OsInfo {
+            ._alloc = alloc,
+            .id = try copied(alloc, id),
+            .version_id = try copied(alloc, version_id),
+            .pretty_name = try copied(alloc, pretty_name),
+            .id_like = try copied(alloc, id_like)
+        };
     }
+
+    pub fn destroy(self:*OsInfo) void {
+        self._alloc.free(self.id);
+        self._alloc.free(self.version_id);
+        self._alloc.free(self.pretty_name);
+        self._alloc.free(self.id_like);
+    }
+};
+
+fn copied(alloc:std.mem.Allocator, value:[]const u8) !*[]u8 {
+    const val_p = try alloc.alloc(u8, value.len);
+    std.mem.copyForwards(u8, val_p, value);
+    return val_p;
 }
 
-pub fn extract_release_info(alloc:std.head.Allocator, lines:std.ArrayList) LineError!OsInfo {
+pub fn extract_release_info(alloc:std.mem.Allocator, lines:std.ArrayList([]const u8)) !OsInfo {
     // All the following are slices into `lines` values
-    const id          = _find_value("ID", lines);
-    const version_id  = _find_value("VERSION_ID", lines);
+    const id          = try find_value("ID", lines);
+    const version_id  = try find_value("VERSION_ID", lines);
 
-    const pretty_name = _find_value_defaulting("PRETTY_NAME", lines, version_id ++ " " ++ id);
-    const id_like     = _find_value_defaulting("ID_LIKE", lines, id);
+    var buffer = [_]u8{undefined}**32;
+    const default_pname = try std.fmt.bufPrint(&buffer, "{s} {s}", .{version_id, id});
+
+    const pretty_name = try find_value_defaulting("PRETTY_NAME", lines, default_pname);
+    const id_like     = try find_value_defaulting("ID_LIKE", lines, id);
 
     // FIXME - this needs to somehow do mem copying into the OsInfo bc otherwise it
     //         must outlive `lines` - which we cannot guarantee from here.
     //         Also, pretty_name can be a stack value if not found in the file
-    return OsInfo.init(alloc){ id:id, version_id:version_id, pretty_name:pretty_name, id_like:id_like };
-
+    return OsInfo.new(alloc, id, version_id, pretty_name, id_like);
 }
 
-fn _find_value(key:[]u8, lines:std.ArrayList) LineError![]u8 {
-    for(lines) |line| {
+// test {
+//     const alloc = std.testing.allocator;
+// 
+//     var lines = std.ArrayList([]const u8).init(alloc);
+//     defer _ = lines.deinit();
+// 
+//     try lines.append("ID=zig\n");
+//     try lines.append("VERSION_ID=0.13.0\n");
+//     try lines.append("ID_LIKE=c\n");
+// 
+//     const info = try extract_release_info(alloc, lines);
+//     defer _ = info.destroy();
+// 
+//     std.debug.print("OsInfo( {s} , {s} , {s} , {s} )\n", .{info.id, info.version_id, info.pretty_name, info.id_like});
+// }
+
+fn find_value(key:[]const u8, lines:std.ArrayList([]const u8)) LineError![]const u8 {
+    var idx:usize = 0;
+    for(lines.items) |line| {
         // if not "=" , return LineError.NotValid
+        idx = std.mem.indexOf(u8, line, "=") orelse 0;
+        if(idx == 0) return LineError.NotValid;
+
         // get slices left and right of "="
+        const left = line[0..idx];
+
         // if left == key, return right
+        if(std.mem.eql(u8, left, key)) return line[idx+1..];
     }
 
     return LineError.NotFound;
 }
 
-fn _find_value_defaulting(key: []u8, lines:std.ArrayList, default:[]u8) LineError.NotValid![]u8 {
-    return _find_value(key, lines) catch default ;
+fn find_value_defaulting(key: []const u8, lines:std.ArrayList([]const u8), default:[]const u8) LineError![]const u8 {
+    return find_value(key, lines) catch |err| {
+        if (err == LineError.NotFound) {
+            return default;
+        } else {
+            return err;
+        }
+    };
+}
+
+
+test {
+    const alloc = std.testing.allocator;
+
+    var lines = std.ArrayList([]const u8).init(alloc);
+    defer _ = lines.deinit();
+
+    try lines.append("NAME=test\n");
+    try lines.append("LANG=zig\n");
+
+    const got_val = try find_value("LANG", lines);
+    try std.testing.expectEqualStrings("zig\n", got_val );
+
+    const def_val = try find_value_defaulting("LING", lines, "zug");
+    try std.testing.expectEqualStrings("zug", def_val );
 }
 
