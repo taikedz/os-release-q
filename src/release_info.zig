a3ea97502aed8048788a21c164ef806e33905b7a
Tai Kedzierski <dch.tai@gmail.com>
2024-10-09 11:33:18 +0100

    Working OsInfo struct
    Remember that slices can be passed around "like pointers", so don't go around
    doing weird pointer notations.
    
    alloc() -> slice , not a pointer to slice !

--- src/release_info.zig
+++ src/release_info.zig
@@ -7,18 +7,23 @@ pub const LineError = error {
 
 pub const OsInfo = struct {
     _alloc:std.mem.Allocator,
-    id:*[]const u8,
-    version_id:*[]const u8,
-    pretty_name:*[]const u8,
-    id_like:*[]const u8,
+    id:[]const u8,
+    version_id:[]const u8,
+    pretty_name:[]const u8,
+    id_like:[]const u8,
 
-    pub fn new(alloc:std.mem.Allocator, id:[]const u8, version_id:[]const u8, pretty_name:[]const u8, id_like:[]const u8) OsInfo {
+    pub fn new(alloc:std.mem.Allocator, id:[]const u8, version_id:[]const u8, pretty_name:[]const u8, id_like:[]const u8) !OsInfo {
+        // For sanity, take responsibility for this struct's own content.
+        // The struct holds references to u8 slices which must persist with it, rather than be
+        //  tied to the lifetime of something that may or may not get deallocated independently.
+        // FIXME - we assign the allocator during `new` , but really we should initialize with OsInfo(alloc){} ?
+        //         We should revisit this later, as a perhaps more advanced topic.
         return OsInfo {
             ._alloc = alloc,
-            .id = try copied(alloc, id),
-            .version_id = try copied(alloc, version_id),
-            .pretty_name = try copied(alloc, pretty_name),
-            .id_like = try copied(alloc, id_like)
+            .id = try own(alloc, id),
+            .version_id = try own(alloc, version_id),
+            .pretty_name = try own(alloc, pretty_name),
+            .id_like = try own(alloc, id_like)
         };
     }
 
@@ -30,9 +35,9 @@ pub const OsInfo = struct {
     }
 };
 
-fn copied(alloc:std.mem.Allocator, value:[]const u8) !*[]u8 {
+fn own(alloc:std.mem.Allocator, value:[]const u8) ![]u8 {
     const val_p = try alloc.alloc(u8, value.len);
-    std.mem.copyForwards(u8, val_p, value);
+    @memcpy(val_p, value);
     return val_p;
 }
 
@@ -42,45 +47,42 @@ pub fn extract_release_info(alloc:std.mem.Allocator, lines:std.ArrayList([]const
     const version_id  = try find_value("VERSION_ID", lines);
 
     var buffer = [_]u8{undefined}**32;
-    const default_pname = try std.fmt.bufPrint(&buffer, "{s} {s}", .{version_id, id});
+    const default_pname = try std.fmt.bufPrint(&buffer, "[{s} {s}]", .{id, version_id});
 
     const pretty_name = try find_value_defaulting("PRETTY_NAME", lines, default_pname);
     const id_like     = try find_value_defaulting("ID_LIKE", lines, id);
 
-    // FIXME - this needs to somehow do mem copying into the OsInfo bc otherwise it
-    //         must outlive `lines` - which we cannot guarantee from here.
-    //         Also, pretty_name can be a stack value if not found in the file
     return OsInfo.new(alloc, id, version_id, pretty_name, id_like);
 }
 
-// test {
-//     const alloc = std.testing.allocator;
-// 
-//     var lines = std.ArrayList([]const u8).init(alloc);
-//     defer _ = lines.deinit();
-// 
-//     try lines.append("ID=zig\n");
-//     try lines.append("VERSION_ID=0.13.0\n");
-//     try lines.append("ID_LIKE=c\n");
-// 
-//     const info = try extract_release_info(alloc, lines);
-//     defer _ = info.destroy();
-// 
-//     std.debug.print("OsInfo( {s} , {s} , {s} , {s} )\n", .{info.id, info.version_id, info.pretty_name, info.id_like});
-// }
+test {
+    const alloc = std.testing.allocator;
+
+    var lines = std.ArrayList([]const u8).init(alloc);
+    defer _ = lines.deinit();
+
+    try lines.append("ID=zig\n");
+    try lines.append("VERSION_ID=0.13.0\n");
+    try lines.append("ID_LIKE=c c++ rust\n");
+
+    var info = try extract_release_info(alloc, lines);
+    defer _ = info.destroy();
+
+    std.debug.print("OsInfo( '{s}' , '{s}' , '{s}' , '{s}' )\n", .{info.id, info.version_id, info.pretty_name, info.id_like});
+}
 
 fn find_value(key:[]const u8, lines:std.ArrayList([]const u8)) LineError![]const u8 {
     var idx:usize = 0;
     for(lines.items) |line| {
-        // if not "=" , return LineError.NotValid
         idx = std.mem.indexOf(u8, line, "=") orelse 0;
         if(idx == 0) return LineError.NotValid;
 
         // get slices left and right of "="
         const left = line[0..idx];
+        const right = std.mem.trim(u8, line[idx+1..], " \r\n");
 
         // if left == key, return right
-        if(std.mem.eql(u8, left, key)) return line[idx+1..];
+        if(std.mem.eql(u8, left, key)) return right;
     }
 
     return LineError.NotFound;
@@ -107,7 +109,7 @@ test {
     try lines.append("LANG=zig\n");
 
     const got_val = try find_value("LANG", lines);
-    try std.testing.expectEqualStrings("zig\n", got_val );
+    try std.testing.expectEqualStrings("zig", got_val );
 
     const def_val = try find_value_defaulting("LING", lines, "zug");
     try std.testing.expectEqualStrings("zug", def_val );
